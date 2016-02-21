e6a963e4c39ee35c69f79b7463ccfc0be8ec5a96
Igor Gumenyuk <me@exslim.net>
2016-02-21 17:40:46 +0200

    Better setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,29 @@ from setuptools import find_packages
 
 VERSION = '1.0.0'
 
+with open('README.rst') as f:
+    readme = f.read()
+
 setup(
     name='visionary',
-    description='Client library for Google Cloud Vision',
+    description='Client library for Google Cloud Vision API',
     url='https://github.com/shafaua/visionary',
     version=VERSION,
+    license="Apache 2.0",
     author=u'Igor Gumenyuk',
+    long_description=readme,
     packages=find_packages(),
     install_requires=[
         'requests<3.0.0',
     ],
     include_package_data=True,
     zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
 )
